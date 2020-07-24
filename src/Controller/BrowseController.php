334a83a8f5b08a9fdc0810bda6fab5cce1bd48e6
MrRisoni <vanguelisk@gmail.com>
2020-07-24 09:29:17 +0300

    first query

--- src/Controller/BrowseController.php
+++ src/Controller/BrowseController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 //use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManager; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class BrowseController
+
+class BrowseController  extends AbstractController
 {
     public function getRestaurants(): void
     {
 
-        $repositoryCusine = $this->getDoctrine()->getRepository(Cuisines::class);
+        $repositoryCusine = $this->getDoctrine()->getRepository(\App\Entity\Cuisines::class);
         
         var_dump($repositoryCusine->findAll());
         echo 'heu';
